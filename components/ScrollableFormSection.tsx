8dd2ead7e53a3a899601b09873905d3010126540
Kelechi Ugwu <kcblack22@gmail.com>
2024-02-08 06:22:53 +0100

    Working on authentication

--- components/ScrollableFormSection.tsx
+++ components/ScrollableFormSection.tsx
@@ -1,21 +1,36 @@
+'use client'
 import { NamedInput, PasswordInput } from "./inputs";
 import LoginButton from "./buttons/LoginButton";
+import { useState } from "react";
 
 const ScrollableFormSection = () => {
+  const [userDetail, setUserDetail] = useState({
+    email:'',
+    password: '',
+    country: '',
+    businessName: '',
+    city: '',
+    name: '',
+    phone: ''
+
+
+  })
   return (
     <>
       <section className='min-w-full h-full flex flex-col py-4 px-[2px] gap-6'>
-        <NamedInput name='Email address' />
-        <NamedInput name='Country' />
-        <PasswordInput name='Password' />
-        <PasswordInput name='Confirm Password' />
+        <NamedInput name='Email address' value={userDetail.email} onChange={(e)=> setUserDetail({...userDetail, email: e.target.value})} />
+        <NamedInput name='Country' value={userDetail.country} onChange={(e)=> setUserDetail({...userDetail, country: e.target.value})}/>
+        <PasswordInput name='Password' value={userDetail.password} onChange={(e)=> setUserDetail({...userDetail, password: e.target.value})} />
+        <PasswordInput name='Confirm Password' value={userDetail.password} onChange={(e)=> setUserDetail({...userDetail, password: e.target.value})} />
       </section>
       <section className='min-w-full h-full flex flex-col py-4 px-[2px] gap-6'>
-        <NamedInput name='Business name' />
-        <NamedInput name='City' />
-        <NamedInput name='Contract Person Name' />
-        <NamedInput name='Phone Number' />
-        <LoginButton text='Create your account' />
+        <NamedInput name='Business name' value={userDetail.businessName} onChange={(e)=> setUserDetail({...userDetail, businessName: e.target.value})} />
+        <NamedInput name='City' value={userDetail.city} onChange={(e)=> setUserDetail({...userDetail, city: e.target.value})} />
+        <NamedInput name='Contract Person Name' value={userDetail.name} onChange={(e)=> setUserDetail({...userDetail, name: e.target.value})} />
+        <NamedInput name='Phone Number' value={userDetail.phone} onChange={(e)=> setUserDetail({...userDetail, phone: e.target.value})} />
+        <LoginButton text='Create your account' 
+        handleSubmit={()=>console.log('Hello Chief')}
+        />
       </section>
     </>
   );
--- components/buttons/LoginButton.tsx
+++ components/buttons/LoginButton.tsx
@@ -1,9 +1,10 @@
 type LoginButtonProps = {
   text: string;
+  handleSubmit: ()=> void;
 }
-const LoginButton = (props?: LoginButtonProps) => {
+const LoginButton = (props: LoginButtonProps) => {
   return (
-    <button className='w-full h-12 mt-5 bg-dispa8chRed-500 rounded-lg text-white font-Inter_Bold text-lg'>
+    <button className='w-full h-12 mt-5 bg-dispa8chRed-500 rounded-lg text-white font-Inter_Bold text-lg' onClick={props.handleSubmit}>
       {props?.text}
     </button>
   );
