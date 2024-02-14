28acf1ee75b29b30612b938ecd9c1c9aa0dd67f1
michTheBrandofficial <michthebrand@gmail.com>
2024-02-13 22:24:27 -0800

    [updated] Hero section large screen animation

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -8,24 +8,21 @@ const HeroSection = () => {
     <section className='w-full min-h-[90vh] px-8 py-6 flex justify-start items-center font-Inter relative z-10 underlay after:bg-hero after:-z-10 lg:py-0 lg:px-16'>
       {/* image */}
       <div
-        className='h-full w-full absolute left-0 top-0 z-0'
-        style={{
-          backgroundImage: `url(${"/images/hero-background.png"})`,
-        }}
+        className='h-full w-full absolute left-0 top-0 z-0 lg:load-bg-image'
       />
       <section className='w-full h-fit column justify-center gap-8 relative z-10 '>
-        <h1 className='font-Inter_Bold text-dispa8chRed-500 text-4xl lg:text-4xl'>
+        <h1 className='font-Inter_Bold text-dispa8chRed-500 text-4xl lg:text-4xl lg:slide-right delay-200'>
           Your one-stop platform that brings <br /> local businesses and direct
           delivery <br /> together
         </h1>
-        <p className='text-lg text-white'>
+        <p className='text-lg text-white lg:slide-right delay-[400ms]'>
           Save time on deliveries, invest more in business growth
         </p>
-        <button className='fit bg-dispa8chRed-500 text-white font-Inter_Bold px-8 py-1 rounded-md '>
+        <button className='fit bg-dispa8chRed-500 text-white font-Inter_Bold px-8 py-1 rounded-md lg:slide-right delay-[600ms] '>
           <Link href={"/dashboard"}>Start free</Link>
         </button>
       </section>
-      <section className='w-fit h-fit hidden lg:block relative z-10'>
+      <section className='w-fit h-fit hidden relative z-10 lg:block lg:slide-left'>
         <img
           src='/images/hero-illustration.png'
           alt='Illustration image'
