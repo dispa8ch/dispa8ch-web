465961753fe21dd38bc09591866a8aaba303357c
king david levi <125862163+kingdavidlevi@users.noreply.github.com>
2024-03-06 02:32:07 -0700

    updated waitlistpage

--- app/WaitListPage/page.tsx
+++ app/WaitListPage/page.tsx
@@ -1,8 +1,14 @@
+"use client";
+
 export default function WaitListPage() {
+  const handleCopy = () => {
+    navigator.clipboard.writeText("");
+    // setButtonText((prev) => !prev);
+  };
   return (
     <>
       <section className='w-full  grid place-items-center bg-gray-300 h-screen '>
-        <div className=' flex flex-col rounded-md justify-center items-center  h-180 bg-white w-200'>
+        <div className=' flex flex-col rounded-md justify-center items-center  py-14 bg-white w-200'>
           <header>
             <h1 className=' mb-3 text-dispa8chRed-500 text-center font-Inter_ExtraBold text-3xl'>
               Thank you for joining our waitlist
@@ -95,26 +101,63 @@ export default function WaitListPage() {
 
           <footer className='mt-6'>
             <div>
-              <p className='text-center mb-6 text-dispa8chRed-500 font-bold   text-base'>
+              <p className='text-center mb-6 text-dispa8chRed-500 font-medium   text-lg'>
                 Let's get your friends, family and colleagues started on this
                 journey too
               </p>
             </div>
 
-            <form>
-              <div className='bg-gray-300 py-2 rounded-md pl-3  w-105'>
+            <form className='grid place-items-center'>
+              <div className='bg-gray-300 flex justify-between py-1.5 pr-3 rounded-md pl-3  w-110'>
                 {" "}
-                <p className='text-gray-500'>
-                  www.dispa8ch.com/jamesking/invite-link
-                </p>
+                <div>
+                  <p className='text-gray-500'>
+                    www.dispa8ch.com/jamesking/invite-link
+                  </p>
+                </div>
+                <div className='mt-2 ' onClick={handleCopy}>
+                  <svg
+                    width='17'
+                    height='10'
+                    viewBox='0 0 17 10'
+                    fill='none'
+                    xmlns='http://www.w3.org/2000/svg'
+                  >
+                    <path
+                      d='M11.8683 1.09351H12.2425C14.3091 1.09351 15.9844 2.8424 15.9844 4.99976C15.9844 7.1571 14.3091 8.90601 12.2425 8.90601H10.7458C8.67924 8.90601 7.00394 7.1571 7.00394 4.99976M5.13302 8.90601H4.75883C2.69228 8.90601 1.01698 7.1571 1.01698 4.99976C1.01698 2.8424 2.69228 1.09351 4.75883 1.09351H6.25557C8.32212 1.09351 9.99742 2.8424 9.99742 4.99976'
+                      stroke='#35000B'
+                      stroke-width='1.7'
+                      stroke-linecap='round'
+                      stroke-linejoin='round'
+                    />
+                  </svg>
+                </div>
               </div>
 
-              <div className='border border-gray-300 py-1.5 rounded-md pl-3  w-105'>
+              <div className='border-b mt-3 mb-8 border-gray-300 py-1.5 pl-3  w-110'>
                 {" "}
-                <p className=' text-center text-lg font-medium text-gray-300'>
+                <p className=' text-center text-base font-medium text-gray-300'>
                   OR
                 </p>
               </div>
+
+              <div className='relative'>
+                <input
+                  placeholder='Enter email to send an invite to your friends'
+                  type='email'
+                  className='bg-gray-300 flex justify-between py-1.5 pr-3 rounded-md pl-3  w-110'
+                />
+                <p className='top-2 text-red-500 text-sm font-medium absolute right-4'>
+                  Send
+                </p>
+              </div>
+
+              <button
+                className='text-white mt-6 font-bold text-xl px-16 py-1.5 rounded-md  bg-dispa8chRed-500'
+                type='submit'
+              >
+                Finish
+              </button>
             </form>
           </footer>
         </div>
