d19e811f81709593596345f542370fcc9e65cee5
michTheBrandofficial <michthebrand@gmail.com>
2024-03-29 15:30:24 -0700

    [feat] Removed test objects and arrays for the blog page

--- app/blog/[slug]/_components/bread-crumbs.tsx
+++ app/blog/[slug]/_components/bread-crumbs.tsx
@@ -1,7 +1,8 @@
 import { cn } from "@/lib/shadcn.utils";
 import { ChevronRightIcon } from "lucide-react";
+import React from "react";
 
-const BreadCrumb = ({ text }: { text: string }) => {
+const BreadCrumb: React.FC<{text: string}>  = ({ text }) => {
   return (
     <p className="text-sm font-Inter_Medium bg-gradient-to-r from-dispa8chRed-500 to-amber-500 text-transparent bg-clip-text " >{text}</p>
   )
@@ -12,15 +13,17 @@ type BreadCrumbsProps = {
   crumbs: string[]
 };
 
-const BreadCrumbs = ({ className, crumbs }: BreadCrumbsProps) => {
+const defaultCrumbs = ['Blog', 'Tech']
+
+const BreadCrumbs: React.FC<BreadCrumbsProps> = ({ className, crumbs }) => {
   const length = crumbs.length
   return (
     <section className={cn("fit flex items-center justify-center gap-2", className || '')}>
-      {crumbs.map((crumb, i) => {
+      {(Boolean(length) ? crumbs : defaultCrumbs).map((crumb, i) => {
         const Comp = () => <BreadCrumb text={crumb} />
         if (length != i + 1) return (<>
           <Comp />
-          <ChevronRightIcon className="stroke-white/20 " size={20} />
+          <ChevronRightIcon className="stroke-black/20 dark:stroke-white/20 " size={20} />
         </>)
         else return <Comp />
       })}
--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -4,8 +4,6 @@ import { Header } from "@/components/dispa8ch-ui";
 import BreadCrumbs from "./_components/bread-crumbs";
 import Comments from "./_components/comments";
 
-const testCrumbs = ["Blog", "Tech", "New-Feature-Release"]
-
 type BlogProps = {} & NextPageProps<{
   slug: string;
 }>;
@@ -13,14 +11,15 @@ type BlogProps = {} & NextPageProps<{
 /**
  * @todo write the UI code for the Main Blog section;
  * should come as markdown
+ * @todo create a parser to parse some json and create the breadcrumbs to be passed to the Breadcrumbs component
  */
 const Blog = ({params}: BlogProps) => {
   return (
     <>
       <Header />
       <main className="h-fit bg-transparent pt-12 px-4 space-y-6 lg:px-8" >
-        <BreadCrumbs className="mr-auto" crumbs={testCrumbs} />
-        <Comments />
+        <BreadCrumbs className="mr-auto" crumbs={[]} />
+        <Comments comments={[]} />
         <section className="w-full column gap-16">
           <h1 className="text-dispa8chRed-600 text-xl mr-auto lg:text-2xl font-Inter_ExtraBold ">Related Blogs</h1>
           <section className="w-full">
