bc1def8143b8d6862555fc76a2e13957725c17e4
michTheBrandofficial <michthebrand@gmail.com>
2024-04-01 15:04:42 -0700

    [fix] Fixed the fix

--- app/blog/[slug]/_components/comment-input.tsx
+++ app/blog/[slug]/_components/comment-input.tsx
@@ -13,8 +13,8 @@ const CommentInput = (props?: CommentInputProps) => {
   return (
     <form action={'/blog'} className="w-full flex gap-6 justify-between items-center ">
       <div className="flex-grow h-fit pb-3 border-b border-b-dispa8chRed-200 text-gray-500 flex gap-2 items-center justify-between ">
-        <input type="text" placeholder="Write your comment..." className="w-full bg-transparent text-[#171717] placeholder:text-gray-500 focus:outline-none dark:text-white/60 " onInput={debounce(({ target: { value } }) => {
-          setValueLength(value.length)
+        <input type="text" placeholder="Write your comment..." className="w-full bg-transparent text-[#171717] placeholder:text-gray-500 focus:outline-none dark:text-white/60 " onInput={debounce(({ target }) => {
+          setValueLength((target as EventTarget & HTMLInputElement).value.length)
         }, 300) as FormEventHandler<HTMLInputElement>} />
         <p>{valueLength}/2000</p>
       </div>
