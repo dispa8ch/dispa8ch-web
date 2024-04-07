220290cf94cb76f51d2dbaadfa60333f97752e53
michTheBrandofficial <michthebrand@gmail.com>
2024-04-06 23:01:49 -0700

    [feat] Animated the sidebar when it is hovered

--- app/dashboard/_components/sidebar.tsx
+++ app/dashboard/_components/sidebar.tsx
@@ -36,7 +36,7 @@ const sidebarLinks: Array<{
 
 const Sidebar = () => {
   return (
-    <aside className="w-fit h-screen relative z-[5] bg-transparent column gap-8 py-3 items-center font-Inter_Medium px-3 border-r border-r-gray-300 transition-[width] duration-700 group " >
+    <aside className="w-[50px] box-content h-screen relative z-[5] bg-transparent column gap-8 py-3 items-center font-Inter_Medium px-3 border-r border-r-gray-300 transition-[width] duration-700 group hover:w-[170px] " >
       <section>
         <Link href={'/'} className="fit block py-1 px-3 border-2 border-dispa8chRed-300 rounded-md">
           <Dispa8chLogoMobile size={.68} />
@@ -46,7 +46,7 @@ const Sidebar = () => {
         {sidebarLinks.map(({ link, icon: Icon, name }, i) => (
           <Link className="w-full py-2 px-3 flex items-center gap-2 rounded-md transition-colors duration-300 hover:bg-gray-300 group-hover:pr-12 " href={link} key={i} >
             <Icon size={1.07} />
-            <p className="text-lg hidden group-hover:block ">{name}</p>
+            <p className={`text-lg hidden group-hover:block `}>{name}</p>
           </Link>
         ))}
       </section>
