45f489c03c98f701d32432de5d031f79a55dd044
michTheBrandofficial <michthebrand@gmail.com>
2024-04-10 10:09:13 -0700

    [feat] 🚀 dashboard/complaints complaints UI is done

---
+++ app/dashboard/complaints/_components/header.tsx
@@ -0,0 +1,9 @@
+const Header: React.FC = () => {
+  return (
+    <section className="w-full pt-2 flex items-center gap-4 font-Inter" >
+      <h1 className="font-Inter_Bold text-2xl text-[#171717]" >Complaints</h1>
+    </section>
+  )
+}
+
+export default Header;
