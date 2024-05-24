fd39bf70adcbd2166e7f64027a258a22855e4f9b
michTheBrandofficial <120549491+michTheBrandofficial@users.noreply.github.com>
2024-05-24 08:38:32 -0700

    [feat] shadcn response dialog added

--- app/dashboard/complaints/page.tsx
+++ app/dashboard/complaints/page.tsx
@@ -1,17 +1,20 @@
-import Complaint from "./_components/complaint"
-import Header from "./_components/header"
-import { complaints } from "./complaints.schema"
+import Complaint from "./_components/complaint";
+import Header from "./_components/header";
+import { complaints } from "./complaints.schema";
 const Complaints: React.FC = () => {
   return (
     <>
       <Header />
-      <section className="w-full mt-4 space-y-4">
-        {complaints.map((complaint,i) => (
-          <Complaint {...complaint} key={i} />
+      <section className='w-full mt-4 space-y-4'>
+        {complaints.map((complaint, i) => (
+          <Complaint
+            {...complaint}
+            key={i}
+          />
         ))}
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Complaints
+export default Complaints;
