fb7fea4bc9366381bdb03efc087a1b6dc769a873
Emmanuel-xt <fattmane5@gmail.com>
2024-07-22 22:50:08 +0100

    completed the Dreivers page

--- app/dashboard/_components/DriverModal.tsx
+++ app/dashboard/_components/DriverModal.tsx
@@ -18,27 +18,50 @@ const DriverModal = () => {
             Add a new driver
           </DialogTitle>
         </DialogHeader>
-
-        <div className="flex gap-4">
-          <div className="">
-            <Label htmlFor="name" className="text-righ t">
-              Name
-            </Label>
-            <Input
-              id="name"
-              defaultValue="Pedro Duarte"
-              className="col-span-3"
-            />
+        <div className="flex flex-col">
+          <div className="flex gap-4">
+            <div className="">
+              <Label htmlFor="name" className="text-righ t">
+                Name
+              </Label>
+              <Input
+                id="name"
+                defaultValue="Pedro Duarte"
+                className="col-span-3"
+              />
+            </div>
+            <div className="">
+              <Label htmlFor="phone" className="text-righ t">
+                Phone Number
+              </Label>
+              <Input
+                id="phone"
+                defaultValue="Pedro Duarte"
+                className="col-span-3"
+              />
+            </div>
           </div>
-          <div className="">
-            <Label htmlFor="phone" className="text-righ t">
-              Phone Number
-            </Label>
-            <Input
-              id="phone"
-              defaultValue="Pedro Duarte"
-              className="col-span-3"
-            />
+          <div className="flex gap-4">
+            <div className="">
+              <Label htmlFor="email" className="text-righ t">
+                Emsil
+              </Label>
+              <Input
+                id="email"
+                defaultValue="eokeke320@gmail.com"
+                className="col-span-3"
+              />
+            </div>
+            <div className="">
+              <Label htmlFor="address" className="text-righ t">
+                Address
+              </Label>
+              <Input
+                id="address"
+                defaultValue="street @ US"
+                className="col-span-3"
+              />
+            </div>
           </div>
         </div>
       </DialogContent>
