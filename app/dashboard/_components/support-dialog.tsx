cf7ec3db580780d1255a0b4c9119d79fb8f04d5a
Emmanuel-xt <fattmane5@gmail.com>
2024-09-24 15:24:02 +0100

    Implement support page

--- app/dashboard/_components/support-dialog.tsx
+++ app/dashboard/_components/support-dialog.tsx
@@ -29,18 +29,20 @@ const SupportDialog: React.FC<Props> = ({ trigger, sender }) => {
       </DialogTrigger>
       <DialogContent className="sm:max-w-md min-h-[40vh] ">
         <DialogHeader>
-          <DialogTitle>Complaint Response</DialogTitle>
+          <DialogTitle className="font-bold">Need Help?</DialogTitle>
         </DialogHeader>
-        <form ref={responseRef} className="flex flex-col items-center gap-2 font-Inter_Medium text-sm">
+        <form ref={responseRef} className="flex flex-col items- gap-2 font-Inter_Medium text-sm">
+          <p className="my-2"> <span className="text-slate-400 ">Call us:</span>  +234 808 768 5678</p>
+          <p className="font-bold">Send us a message</p>
           <section className="w-full flex items-center gap-2">
             <button disabled className="w-1/2 h-fit py-2.5 pl-3.5 text-start bg-white border border-gray-200 rounded-lg text-gray-400 " tabIndex={1}>
               From: <span className="text-gray-500">{sender}</span>
             </button>
             <button disabled className="w-1/2 h-fit py-2.5 pl-3.5 text-start bg-white border border-gray-200 rounded-lg text-gray-400 " tabIndex={1}>
-              From: <span className="text-gray-500">Dispa8ch Inc.</span>
+              To: <span className="text-gray-500">Dispa8ch Inc.</span>
             </button>
           </section>
-          <Textarea name="response" rows={10} placeholder="Enter your response" className="placeholder:text-gray-300 " ></Textarea>
+          <Textarea name="response" rows={10} placeholder="Enter your message" className="placeholder:text-gray-300 " ></Textarea>
         </form>
         <DialogFooter className="sm:justify-end ">
           <DialogClose asChild>
